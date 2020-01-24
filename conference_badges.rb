57394a5fe97f8380d37ef5dad745aebd9adc694f
Angela Appiah <alegnalawn2@yahoo.com>
2020-01-24 01:27:37 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,9 @@
 # Write your code here.
+badge_maker ( "Edsger", "Ada", "Charles", "Alan", "Grace", "Linux", "Martz").
+each do. # (badge_maker) 
+puts 'Hello, my name is # "( badge_maker)".
+end
+square_array numbers (1,2,3,4,5,6,7)
+assign_room.each do #( square_array numbers).
+'puts " Hello, my name is "( badge_maker)" ! You will be assigned to room "(square_array)"!.
+end
